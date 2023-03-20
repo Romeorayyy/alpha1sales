f14f87caf86bf8fc2e9c4bcd43d0e070d70c7dcc
Randy Yono <randyyono96@gmail.com>
2023-03-19 21:55:27 -0400

    changed the path of the photos to come from a url instead of locally

--- client/src/products.js
+++ client/src/products.js
@@ -1,67 +1,58 @@
-import product1 from "./assets/products1/1.png";
-import product2 from "./assets/products1/2.jpg";
-import product3 from "./assets/products1/3.png";
-import product4 from "./assets/products1/4.png";
-import product5 from "./assets/products1/5.png";
-import product6 from "./assets/products1/6.webp";
-import product7 from "./assets/products1/7.webp";
-import product8 from "./assets/products1/8.webp";
-
 export const PRODUCTS = [
   {
     id: 1,
     productName: "randy",
     price: 45.0,
-    productImage: product1,
+    productImage: "https://drive.google.com/uc?id=14KxBvi04VTVnG8Ki1GeTUcN5U4u08NKT",
     description: "this is the description for product 1"
   },
   {
     id: 2,
     productName: "Vape 2",
     price: 45.0,
-    productImage: product2,
+    productImage: "https://drive.google.com/uc?id=15K4kb4FkS3ilF2ipA_vlu_y7ij7Iqc5W",
     description: "this is the description for product 2"
   },
   {
     id: 3,
     productName: "Vape 3",
     price: 45.0,
-    productImage: product3,
+    productImage: "https://drive.google.com/uc?id=1NwfISi9ONUHqx1bEZt7RAZuqtQxHqkeJ",
     description: "this is the description for product 3"
   },
   {
     id: 4,
     productName: "Vape 4",
     price: 45.0,
-    productImage: product4,
+    productImage: "https://drive.google.com/uc?id=1UKKalxQonrPWiNJz8VjU_N6d49vSnZou",
     description: "this is the description for product 4"
   },
   {
     id: 5,
     productName: "Vape 5",
     price: 45.00,
-    productImage: product5,
+    productImage: "https://drive.google.com/uc?id=1HrzuDBhaWyEmanr2jt0P4AuSZaYibHzJ",
     description: "this is the description for product 5"
   },
   {
     id: 6,
     productName: "Vape 6",
     price: 45.0,
-    productImage: product6,
+    productImage: "https://drive.google.com/uc?id=137MwcZxWYaidp9QZhXSTa_9Df1lU682t",
     description: "this is the description for product 6"
   },
   {
     id: 7,
     productName: "Vape 7",
     price: 45.0,
-    productImage: product7,
+    productImage: "https://drive.google.com/uc?id=1zmOSKhGVw1pajKWcMEzZLrvuWNkAYRfs",
     description: "this is the description for product 7"
   },
   {
     id: 8,
     productName: "Vape 8",
     price: 45.0,
-    productImage: product8,
+    productImage: "https://drive.google.com/uc?id=1zmOSKhGVw1pajKWcMEzZLrvuWNkAYRfs",
     description: "this is the description for product 8"
   },
 ];
