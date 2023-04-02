3900bcd94bbddeae817073a74114622276feb509
Randy Yono <randyyono96@gmail.com>
2023-04-01 22:06:19 -0400

    updated the serverless function in send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -58,18 +58,13 @@ async function sendSMS(to, message) {
 }
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const { method } = req;
-
   if (method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.status(200).end();
-    return;
-  }
-
-  if (method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
     return;
   }
 
