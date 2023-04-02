b52bdcc417e5fec8a613019f831bd3481ef4314e
Randy Yono <randyyono96@gmail.com>
2023-04-01 23:48:53 -0400

    another update

--- api/send-email.js
+++ api/send-email.js
@@ -59,14 +59,10 @@ async function sendSMS(to, message) {
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST'); // Only allow POST method
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   const { method } = req;
-  if (method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
 
   const { query } = parse(req.url, true);
   const { email, firstName, lastName, companyName, taxId, phoneNumber, address, cartItems, totalAmount } = req.body;
